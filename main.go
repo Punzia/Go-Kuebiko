5921e69725189bd9ea779fb12170b3d8b7de58c9
Punzia <28727157+Punzia@users.noreply.github.com>
2022-12-05 16:07:35 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,73 +1,20 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"go-kuebiko/kuebiko"
+	"log"
 	"os"
-	"os/signal"
-	"strings"
-	"syscall"
-
-	"github.com/bwmarrin/discordgo"
 )
-
-var (
-	Token string
-)
-
-func init() {
-
-	flag.StringVar(&Token, "t", "", "Bot Token")
-	flag.Parse()
-}
 
 func main() {
-
-	// Create a new Discord session using the provided bot token.
-	dg, err := discordgo.New("Bot " + Token)
-	if err != nil {
-		fmt.Println("error creating Discord session,", err)
-		return
+	// Load environment variables
+	botToken, ok := os.LookupEnv("BOT_TOKEN")
+	if !ok {
+		log.Fatal("Must set Discord token as env variable: BOT_TOKEN")
 	}
 
-	// Register the messageCreate func as a callback for MessageCreate events.
-	dg.AddHandler(ready)
-	dg.AddHandler(messageCreate)
-
-	// In this example, we only care about receiving message events.
-	dg.Identify.Intents = discordgo.IntentsGuilds | discordgo.IntentsGuildMessages | discordgo.IntentsGuildVoiceStates
-
-	// Open a websocket connection to Discord and begin listening.
-	err = dg.Open()
-	if err != nil {
-		fmt.Println("error opening connection,", err)
-		return
-	}
-
-	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
-
-	// Cleanly close down the Discord session.
-	dg.Close()
-}
-func ready(s *discordgo.Session, event *discordgo.Ready) {
-
-	// Set the playing status.
-	s.UpdateGameStatus(0, "!golang")
-}
-
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	fmt.Println(m.Content)
-	// Don't respond to himself
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-
-	if strings.HasPrefix(m.Content, "!golang") {
-		s.ChannelMessageSend(m.ChannelID, "Hey! I'm using golang!")
-	}
+	// Start the bot
+	kuebiko.BotToken = botToken
+	kuebiko.Run()
 
 }
