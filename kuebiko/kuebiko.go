83a9feb0d9bf1c379c69d82dca3d5b45e59e96d0
Punzia <28727157+Punzia@users.noreply.github.com>
2023-01-12 18:35:27 +0100

    Update kuebiko.go

--- kuebiko/kuebiko.go
+++ kuebiko/kuebiko.go
@@ -4,12 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/url"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/darenliang/jikan-go"
 )
 
 // Bot paremeters
@@ -19,73 +22,226 @@ var (
 	///GuildID  = ""
 	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
 )
-var s *discordgo.Session
+
+//var s *discordgo.Session
 
 var commands = []*discordgo.ApplicationCommand{
 	{
-		Name:        "basic-command",
-		Description: "Kuebiko Responds!",
-	},
-	{
-		Name:        "animelist",
-		Description: "get AnimeList",
+		Name:        "anime",
+		Description: "Get the Anime from MyAnimeList",
 		// String option here ->
 		Options: []*discordgo.ApplicationCommandOption{
 			{
 				Type:        discordgo.ApplicationCommandOptionString,
-				Name:        "username",
-				Description: "Type the username of the anime list",
+				Name:        "name",
+				Description: "Type the anime name",
 				Required:    true,
 			},
 		},
 	},
 	{
-		Name:        "コード",
-		Description: "Bla",
+		Name:        "kuebiko",
+		Description: "Get senpai to notice you.",
 	},
+	{
+		Name:        "random-anime",
+		Description: "Get a random anime!",
+	},
+	//https://media.tenor.com/NL_qdSSs28EAAAAd/eevee-dancing.gif
 }
 
 var commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-	"basic-command": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "Hey there! Congratulations, you just executed your first slash command",
-			},
-		})
-	},
-	"animelist": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	"anime": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		options := i.ApplicationCommandData().Options
 
 		optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
+
 		for _, opt := range options {
 			optionMap[opt.Name] = opt
 		}
-		output := optionMap["username"]
 
-		//margs := make([]interface{}, 0, len(options))
+		output := optionMap["name"]
+		//fmt.Printf(output.Name)
+
+		query := url.Values{}
+		fmt.Println("Input: ", output.StringValue())
+		query.Set("q", output.StringValue())
+		//query.Set("type", "tv")
+
+		// Search anime
+		search, err := jikan.GetAnimeSearch(query)
+		if err != nil {
+			panic(err)
+		}
+		animeData := search.Data[0]
+		animeYear := fmt.Sprintf("%s (%d)", strings.Title(animeData.Season), animeData.Year)
+		animeTitle := fmt.Sprintf("%s (%s)", animeData.TitleEnglish, (animeData.TitleJapanese))
+		animeGenre := formatGenre(animeData.Genres)
+		animeRanking := fmt.Sprintf("#%s", strconv.Itoa(animeData.Rank))
+		animeSym := word_limiter(animeData.Synopsis, 180)
+		fmt.Printf(animeSym)
+		//animeDesc := animeData.Background
+		fmt.Printf(animeGenre)
+
+		//animeYear := `${animeData.Season}, animeData.Year`
+
 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
-				Content: output.StringValue(),
+				Embeds: []*discordgo.MessageEmbed{
+					{
+						Author: &discordgo.MessageEmbedAuthor{},
+						Title:  animeTitle,
+						Color:  0xb004b2, // Green
+						/*
+							https://github.com/cadecuddy/sauce/blob/main/utils/print.go
+						*/
+						Fields: []*discordgo.MessageEmbedField{
+							{
+								Name:   "❓ Type⤵",
+								Value:  animeData.Type,
+								Inline: true,
+							},
+							{
+								Name:   "🕐 Episodes⤵",
+								Value:  strconv.Itoa(animeData.Episodes),
+								Inline: true,
+							},
+							{
+								Name:   "💡 Status⤵",
+								Value:  animeData.Status,
+								Inline: true,
+							},
+							{
+								Name:   "🎥 Studios⤵",
+								Value:  animeData.Studios[0].Name,
+								Inline: true,
+							},
+							{
+								Name:   "📅 Year⤵",
+								Value:  animeYear,
+								Inline: true,
+							},
+							{
+								Name:   "📈 Score⤵",
+								Value:  strconv.FormatFloat(animeData.Score, 'g', 5, 64),
+								Inline: true,
+							},
+							{
+								Name:   "🏆 Ranking⤵",
+								Value:  animeRanking,
+								Inline: true,
+							},
+							{
+								Name:   "📕 Source⤵",
+								Value:  animeData.Source,
+								Inline: true,
+							},
+							{
+								Name:   "📜 Genres⤵",
+								Value:  animeGenre,
+								Inline: true,
+							},
+							{
+
+								Name:   "Description⤵",
+								Value:  animeData.Synopsis,
+								Inline: false,
+							},
+						},
+						Thumbnail: &discordgo.MessageEmbedThumbnail{
+							URL: animeData.Images.Jpg.LargeImageUrl,
+						},
+					},
+				},
 			},
 		})
 
 	},
-	"コード": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	"kuebiko": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
-				Content: "たわけ者",
+				Content: "https://media.tenor.com/NL_qdSSs28EAAAAd/eevee-dancing.gif",
 			},
 		})
+	},
+	"random-anime": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 
+		// Search anime
+		getRandom, err := jikan.GetRandomAnime()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(getRandom.Data.Title)
+		gotRandom := getRandom.Data
+
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Embeds: []*discordgo.MessageEmbed{
+					{
+						Author:      &discordgo.MessageEmbedAuthor{},
+						Title:       gotRandom.Title,
+						Color:       0xb004b2, // Green
+						Description: gotRandom.Synopsis,
+						Thumbnail: &discordgo.MessageEmbedThumbnail{
+							URL: gotRandom.Images.Jpg.LargeImageUrl,
+						},
+					},
+				},
+			},
+		})
 	},
 }
 
+func formatGenre(genres []jikan.MalItem) string {
+	var result string
+
+	for i, genre := range genres {
+		if i != 0 {
+			//result += genre.Name
+			result += fmt.Sprintf(", %s", genre.Name)
+			//result += (", %s", genre.Name)
+			//result := (", ", genre.Name)
+			//o:= (", ", genre.Name)
+		} else {
+			result += genre.Name
+		}
+	}
+
+	//r := fmt.Sprintf("%s", o)
+	return result
+	//return out
+}
+func word_limiter(s string, limit int) string {
+
+	if strings.TrimSpace(s) == "" {
+		return s
+	}
+
+	// convert string to slice
+	strSlice := strings.Fields(s)
+
+	// count the number of words
+	numWords := len(strSlice)
+
+	var result string
+
+	if numWords > limit {
+		// convert slice/array back to string
+		result = strings.Join(strSlice[0:limit], " ")
+		result = result + "..."
+	} else {
+
+		result = s
+	}
+
+	return string(result)
+
+}
+
 func Run() {
-	//fmt.Println("Got Tokens: ", BotToken)
-	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + BotToken)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
@@ -95,16 +251,12 @@ func Run() {
 	// Register the handlers
 	dg.AddHandler(ready)
 	dg.AddHandler(messageCreate)
-	//dg.AddHandler(commandHandler)
 
 	dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
 			h(s, i)
 		}
 	})
-
-	//dg.AddHandler(messageCreate)
-
 	// Add the intents for the bot!
 	dg.Identify.Intents = discordgo.IntentsGuilds | discordgo.IntentsGuildMessages | discordgo.IntentsGuildVoiceStates
 
@@ -126,17 +278,17 @@ func Run() {
 	dg.Close()
 }
 func ready(s *discordgo.Session, event *discordgo.Ready) {
-
-	log.Println("Adding commands...")
-	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
-	// Adds the commands
-	for i, v := range commands {
-		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, *GuildID, v)
-		if err != nil {
-			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
-		}
-		registeredCommands[i] = cmd
-	}
+	/*
+		log.Println("Adding commands...")
+		registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+		// Adds the commands
+		for i, v := range commands {
+			cmd, err := s.ApplicationCommandCreate(s.State.User.ID, *GuildID, v)
+			if err != nil {
+				log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+			}
+			registeredCommands[i] = cmd
+		}*/
 	/*
 		if *RemoveCommands {
 			log.Println("Removing commands...")
@@ -149,7 +301,7 @@ func ready(s *discordgo.Session, event *discordgo.Ready) {
 			}
 		}*/
 	// Set the playing status.
-	s.UpdateGameStatus(0, "!golang")
+	s.UpdateGameStatus(2, "!golang")
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
